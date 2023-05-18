2dac68199c37a4277e11818961b6114cc0f34896
saminshakya <saminshakya27@gmail.com>
2023-05-18 12:39:06 +0545

    Basic storyboard vc created

---
+++ RandomColors/ColorsTableVC.swift
@@ -0,0 +1,32 @@
+//
+//  ColorsTableVC.swift
+//  RandomColors
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 18/05/2023.
+//
+
+import UIKit
+
+class ColorsTableVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tempButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
