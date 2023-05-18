e74c687270571899a9eab0e26a7999278dac7b36
saminshakya <saminshakya27@gmail.com>
2023-05-18 12:15:01 +0545

    Project setup

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 18/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
